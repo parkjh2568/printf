82328086e72f27de1a941e6dc6230b146e213749
Junhyun Park <junhypar@student.42seoul.kr>
2020-07-25 16:55:35 +0900

    reapir

--- ft_printf.c
+++ ft_printf.c
@@ -6,74 +6,90 @@
 /*   By: junhypar <junhypar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/25 12:27:44 by junhypar          #+#    #+#             */
-/*   Updated: 2020/07/25 16:05:03 by junhypar         ###   ########.fr       */
+/*   Updated: 2020/07/25 16:55:27 by junhypar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int		config_type(const char *input, int i, int j)
+int		config_type(const char *input, int i, int *j)
 {
-	while(j++)
+	while ((*j = *j + 1))
 	{
-		if (!(input[i + j] >= '0' && input[i + j] <= '9') || input[i] != '.'
-				|| input[i + j] != '-' || input[i + j] != '*')
-			break;
+		if (!(input[i + *j] >= 48 && input[i + *j] <= 57) || input[i + *j] != 78
+				|| input[i + *j] != '-' || input[i + *j] != '*')
+			break ;
 	}
-	if (input[i + j] == '%')
+	if (input[i + *j] == '%')
 		return (1);
-	else if (input[i + j] == 'c')
+	else if (input[i + *j] == 'c')
 		return (2);
-	else if (input[i + j] == 's')
+	else if (input[i + *j] == 's')
 		return (3);
-	else if (input[i + j] == 'p')
+	else if (input[i + *j] == 'p')
 		return (4);
-	else if (input[i + j] == 'd')
+	else if (input[i + *j] == 'd')
 		return (5);
-	else if (input[i + j] == 'i')
+	else if (input[i + *j] == 'i')
 		return (6);
-	else if (input[i + j] == 'u')
+	else if (input[i + *j] == 'u')
 		return (7);
-	else if (input[i + j] == 'x')
+	else if (input[i + *j] == 'x')
 		return (8);
-	else if (input[i + j] == 'X')
+	else if (input[i + *j] == 'X')
 		return (9);
 	return (0);
 }
 
-int		config_type2(const char *input, int i)
+int		print_pp(void)
 {
-
+	write(1, "%", 1);
+	return (1);
 }
 
-int		print_%%()
+int		config_type2(int i, const char *input)
 {
-	if (config_type(input, i, 0) == 1)
-	{
-		write(1,"%",1);
-		w++;
-		i++;
-	}
+	if (i == 1)
+		return (print_pp());
+/*	else if (i == 2)
+		return ();
+	else if (i == 3)
+		return ();
+	else if (i == 4)
+		return ();
+	else if (i == 5)
+		return ();
+	else if (i == 6)
+		return ();
+	else if (i == 7)
+		return ();
+	else if (i == 8)
+		return ();
+	else if (i == 9)
+		return ();	*/
+	return (0);
 }
 
-int		search_word(const char *input, va_list lst, char *word)
+int		search_word(const char *input, va_list lst, char *word, int w)
 {
-	int i, w;
+	int i, j;
 
 	i = 0;
-	w = 0;
-	while(input[i])
+	while (input[i])
 	{
+		j = 0;
 		if (input[i] == '%' && input[i + 1])
 		{
-			if (config_type(input, i, 0) > 0)
+			if (config_type(input, i, &j) > 0)
 			{
-				config_type2(
+				j = 0;
+				w += config_type2(config_type(input, i, &j), input);
+				i += j;
 			}
 		}
 		else
 		{
-			write(1,&input[i],1);
+			write(1, &input[i], 1);
 			w++;
 		}
 		i++;
@@ -81,12 +97,12 @@ int		search_word(const char *input, va_list lst, char *word)
 	return (w);
 }
 
-int ft_printf(const char *input, ...)
+int		ft_printf(const char *input, ...)
 {
 	va_list	lst;
 	char	*word;
 
 	word = ft_strdup((char *)input);
 	va_start(lst, input);
-	return (search_word(input,lst,word));
+	return (search_word(input, lst, word, 0));
 }
